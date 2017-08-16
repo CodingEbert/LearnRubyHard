682c225b97b8de7881557cbcf5936a2fadbf2b21
Ebert Mahon <ebertmahon@gmail.com>
2017-08-16 21:19:03 +0000

    added ex5

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+name = 'Ebert A. Mahon'
+age = 58 # not a lie in 2009
+height = 72 # inches
+weight = 176 # lbs
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+#this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
